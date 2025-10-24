d96722a6e863f29e7694a72a1a846a9f6ff2cf1e
Ely Deckers <e.deckers@gmail.com>
2025-10-24 19:48:38 +0200

    refactor: disconnect session creation from auth flow

--- src/lib/Session.ts
+++ src/lib/Session.ts
@@ -1,5 +1,9 @@
-import { SecretKey } from "./Account/SecretKey";
-import { Vault } from "./Vault/Vault";
+import { KeysetRepository } from "~/lib/Account/AccountRepository";
+import { AccountUnlockKey } from "~/lib/Account/AccountUnlockKey";
+import { SecretKey } from "~/lib/Account/SecretKey";
+import { KeysetDecryptor } from "~/lib/Keysets/KeysetDecryptor";
+import { Vault } from "~/lib/Vault/Vault";
+import { VaultRepository } from "~/lib/Vault/VaultRepository";
 
 export class Session {
   constructor(
@@ -9,4 +13,26 @@ export class Session {
 
   public getPersonalVault = (): Vault | undefined =>
     this.vaults.length > 0 ? this.vaults[0] : undefined;
+
+  public static using = (
+    keysetRepository: KeysetRepository,
+    vaultRepository: VaultRepository,
+  ) => ({
+    start: async (
+      accountUnlockKey: AccountUnlockKey,
+      secretKey: SecretKey,
+    ): Promise<Session> => {
+      const keysets = await keysetRepository.readKeysetsRaw();
+
+      const decryptor = await KeysetDecryptor.unlock(accountUnlockKey, keysets);
+
+      const vaults = await vaultRepository.list();
+
+      const unlockedVaults = await Promise.all(
+        vaults.map((v) => Vault.using(vaultRepository).unlock(decryptor, v)),
+      );
+
+      return new Session(secretKey, unlockedVaults);
+    },
+  });
 }
