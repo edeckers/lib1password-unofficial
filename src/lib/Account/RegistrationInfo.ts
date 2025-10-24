3c2122ffd3063dd6944ea589d0855f472635a63f
Ely Deckers <e.deckers@gmail.com>
2025-10-24 09:44:15 +0200

    fix: mov aukSalt from RegistrationInfo to AccountCreator (#2)

--- src/lib/Account/RegistrationInfo.ts
+++ src/lib/Account/RegistrationInfo.ts
@@ -1,20 +1,17 @@
 import { AccountUnlockKey } from "~/lib/Account/AccountUnlockKey";
 import { SecretKey } from "~/lib/Account/SecretKey";
-import { generateSalt } from "~/lib/Encryption";
 
 export class RegistrationInfo {
   constructor(
     public readonly emailAddress: string,
     public readonly secretKey: SecretKey,
     public readonly auk: AccountUnlockKey,
-    public readonly aukSalt: Uint8Array<ArrayBuffer>,
   ) {}
 
   public static create = async (
     emailAddress: string,
     password: string,
   ): Promise<RegistrationInfo> => {
-    const aukSalt = generateSalt();
     const secretKey = SecretKey.generate();
 
     const auk = await AccountUnlockKey.create(
@@ -27,7 +24,6 @@ export class RegistrationInfo {
       emailAddress,
       secretKey,
       auk,
-      aukSalt,
     };
   };
 }
