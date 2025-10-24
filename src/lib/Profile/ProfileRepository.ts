b6c0db251c405474416996d6640675bd443e4621
Ely Deckers <e.deckers@gmail.com>
2025-10-24 17:58:14 +0200

    fix: leaky abstraction of ProfileAuth

--- src/lib/Profile/ProfileRepository.ts
+++ src/lib/Profile/ProfileRepository.ts
@@ -1,20 +1,6 @@
-import { KEY_DERIVATION_NUMBER_OF_ITERATIONS } from "~/Consts";
-import { Profile, ProfileAuth } from "~/lib/Profile/Entities";
-import { base64encode } from "~/lib/Encoding";
+import { Profile } from "~/lib/Profile/Entities";
 
 const accountKeyFormat = "A3";
-const alg = "PBES2g-HS256";
-const iterations = KEY_DERIVATION_NUMBER_OF_ITERATIONS;
-const method = "SRPg-4096";
-
-export const createProfileAuth = (
-  saltBytes: Uint8Array<ArrayBuffer>,
-): ProfileAuth => ({
-  salt: base64encode(saltBytes),
-  alg,
-  iterations,
-  method,
-});
 
 export const createProfile = (accountKeyUuid: string): Profile => ({
   accountKeyUuid,
